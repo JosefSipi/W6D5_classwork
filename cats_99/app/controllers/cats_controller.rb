954299d257ee9ecbfb2e452d2a0aa88f0c0a327e
Joseph <Josephsipiorski@gmail.com>
2021-04-03 12:57:57 -0500

    finished Phase 1

--- cats_99/app/controllers/cats_controller.rb
+++ cats_99/app/controllers/cats_controller.rb
@@ -17,6 +17,30 @@ class CatsController < ApplicationController
         render :new
     end
 
+    def edit
+        @cat = Cat.find(params[:id])
+        render :edit
+    end
+
+    def update
+        @cat = Cat.find(params[:id])
+        if @cat.update_attributes(cat_params)
+            redirect_to cat_url(@cat)
+        else
+            flash.now[:errors] = @cat.errors.full_messages
+            render :edit
+        end
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+        end
+
+    end
 
 
 
